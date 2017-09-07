fca877e9acfba52d2a67f826e56f958a525634cf
gjguanjie <gjguanjie@sina.com>
2017-09-07 19:41:25 +0800

    Add files via upload
    增加 前后端请求 跨域问题

---
+++ amateur-zuul/src/main/java/com/cloud/config/CorsConfig.java
@@ -0,0 +1,35 @@
+package com.cloud.config;/**
+ * Created by guanjie-002 on 2017/9/7.
+ */
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * @author guanjie
+ * @create 2017-09-07 19:03
+ **/
+@Configuration
+public class CorsConfig {
+
+    private CorsConfiguration buildConfig() {
+
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOrigin("*");
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("*");
+        return configuration;
+
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**",buildConfig());
+        return new CorsFilter(source);
+    }
+
+}
